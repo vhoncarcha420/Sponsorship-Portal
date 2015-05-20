0f97b9d5cb040ae5c733ec0a0b723c9972ef4560
Von <vhoncarcha420@gmail.com>
2015-05-20 13:39:26 +0800

    products included

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
-TweenLite.set("#overlay", {x:0, xPercent:100});
-TweenLite.set("#overlay-2", {x:0, xPercent:100});
+TweenLite.set("#product-wrapper", {x:0, xPercent:100});
+TweenLite.set("#plan-wrapper", {x:0, xPercent:100});
 
 $('#open').on('click',
   function() {
-    TweenMax.to("#overlay", 0.5, {xPercent: 0, ease:Power4.easeInOut});
+    TweenMax.to("#product-wrapper", 0.5, {xPercent: 0, ease:Power4.easeInOut});
   });
 
 $('#close').on('click',
   function() {
-    TweenMax.to("#overlay", 0.5, {xPercent: 100, ease:Power4.easeInOut});
+    TweenMax.to("#product-wrapper", 0.5, {xPercent: 100, ease:Power4.easeInOut});
   });
 
 $('#open-2').on('click',
   function() {
-    TweenMax.to("#overlay-2", 0.5, {xPercent: 0, ease:Power4.easeInOut});
+    TweenMax.to("#plan-wrapper", 0.5, {xPercent: 0, ease:Power4.easeInOut});
   });
 
 $('#close-2').on('click',
   function() {
-    TweenMax.to("#overlay-2", 0.5, {xPercent: 100, ease:Power4.easeInOut});
+    TweenMax.to("#plan-wrapper", 0.5, {xPercent: 100, ease:Power4.easeInOut});
   });
+
+
+
+
+
+
